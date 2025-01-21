61920b1492a5408cd57b5481578d35a7ab0beb10
MinhTai <phanminhtai23@gmail.com>
2025-01-22 04:08:47 +0700

    collecting Drug-Drug Interaction

--- func/CRAFT/craft.py
+++ func/CRAFT/craft.py
@@ -79,7 +79,7 @@ class CRAFT(nn.Module):
 
         return y.permute(0,2,3,1), feature
 
-if __name__ == '__main__':
-    model = CRAFT(pretrained=True).cuda()
-    output, _ = model(torch.randn(1, 3, 768, 768).cuda())
-    print(output.shape)
+# if __name__ == '__main__':
+#     model = CRAFT(pretrained=True).cuda()
+#     output, _ = model(torch.randn(1, 3, 768, 768).cuda())
+#     print(output.shape)
